62de044eb2aaa64042ee3f47def0841ec5d889ea
arsimr16 <arsimr16@g.holycross.edy>
2017-08-22 11:01:33 -0700

    pass implemented tests for part 1

--- src/underbar.js
+++ src/underbar.js
@@ -115,6 +115,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var output = [];
+    _.each(collection, function(value) {
+      output.push(iterator(value));
+    });
+    return output;
   };
 
   /*
@@ -156,6 +161,14 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (accumulator === undefined) {
+      accumulator = collection[0];
+      collection = collection.slice(1);
+    }
+    _.each(collection, function(value) {
+      accumulator = iterator(accumulator, value);
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
